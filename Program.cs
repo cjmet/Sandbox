783b21bfaaa34a2a47a628b32ab62e46ead823e5
Christian J Metcalfe <khaaiev@gmail.com>
2024-01-23 23:29:36 -0500

    Update 240123

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+CancellationToken ct = cts.Token;
+
+try
+{
+    Task.Run(() =>
+    {
+        int i = 0;
+        while (true)
+        {
+            Task.Delay(1000).Wait();
+            Console.WriteLine($"{++i,2}:Hello, World!");
+            // if(ct.IsCancellationRequested) return;
+        }
+    }).Wait(TimeSpan.FromSeconds(5),ct);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Canceled!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Waiting for 5 seconds...");
+Task.Delay(5000).Wait();
+Console.WriteLine("All Done.");
