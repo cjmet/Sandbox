dda0191121ca706b93039975fa26f04f1617c671
CJ Metcalfe <khaaiev@gmail.com>
2024-02-20 22:12:59 -0500

    * Worked on the second Maui Window to display AhLog() data.  Not Done Yet, but got a start.

--- Program.cs
+++ Program.cs
@@ -6,48 +6,62 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using AngelHornetLibrary;
 
-bool loop = true;
-string _progressString = "Starting Search...";
-Progress<string> progress = new Progress<string>(s => _progressString = s); ;
-
-Task task = new Task(async () =>
-{
-    int i = 0;
-    List<string> paths = ["N:\\Archive", "N:\\FTP", "N:\\Public", "N:\\Temp", "N:\\Updates", "N:\\Work"];
-    await foreach (var result in new AhGetFiles().GetFilesAsync(paths, "*.mp3", progress: progress))
-    {
-        string s = $"\rF[{++i,4}]  {result}";
-        s = s.PadRight(Console.BufferWidth);
-        Console.WriteLine(MiddleTruncate(s));
-    }
-    Console.WriteLine($"Search Complete. [{i}] files found.");
-    loop = false;
-});
-task.Start();
-
-{
-    int j = 0;
-    int spinner = 0;
-    do
-    {
-
-        // spin for a while
-        var spin = "|/-\\"[spinner++ % 4];
-        string s = $"\rS[{j++/4,4}]{spin} {_progressString}";
-        s = s.PadRight(Console.BufferWidth);
-        Console.Write(MiddleTruncate(s));
-        Thread.Sleep(250);
-    } while (loop);
-    Console.Write($"S[{(j/4),4}]");
-}
-Console.ReadLine();
-Environment.Exit(0);
 
 
+//bool TEST_N_DRIVE = true;
+//if (TEST_N_DRIVE)
+//{
+//    bool loop = true;
+//    string _progressString = "Starting Search...";
+//    Progress<string> progress = new Progress<string>(s => _progressString = s); ;
+
+//    Task task = new Task(async () =>
+//    {
+//        int i = 0;
+//        List<string> paths = ["N:\\Archive", "N:\\FTP", "N:\\Public", "N:\\Temp", "N:\\Updates", "N:\\Work"];
+//        await foreach (var result in new AhGetFiles().GetFilesAsync(paths, "*.mp3", progress: progress))
+//        {
+//            string s = $"\rF[{++i,4}]  {result}";
+//            s = s.PadRight(Console.BufferWidth);
+//            Console.WriteLine(MiddleTruncate(s));
+//        }
+//        Console.WriteLine($"Search Complete. [{i}] files found.");
+//        loop = false;
+//    });
+//    task.Start();
+
+//    {
+//        int j = 0;
+//        int spinner = 0;
+//        do
+//        {
+
+//            // spin for a while
+//            var spin = "|/-\\"[spinner++ % 4];
+//            string s = $"\rS[{j++ / 4,4}]{spin} {_progressString}";
+//            s = s.PadRight(Console.BufferWidth);
+//            Console.Write(MiddleTruncate(s));
+//            Thread.Sleep(250);
+//        } while (loop);
+//        Console.Write($"S[{(j / 4),4}]");
+//    }
+//    Console.ReadLine();
+//    Environment.Exit(0);
+//}
+// / TEST_N_DRIVE
+
+
+
+
+// Aync DEMO
+// Test the Asynchronous Interface DEMO
 
 mainloop();
 Environment.Exit(0);
 
+
+
+// Main Loop, Simulating a GUI Application
 async void mainloop()
 {
     int spinner = 0;
@@ -70,7 +84,7 @@ async void mainloop()
     Console.WriteLine(message);
 }
 
-// Async Task or Async Void, since this is  in theory simulating a button click event
+// Async Task or Async Void, since this is in theory simulating a button click event
 async Task ButtonClickedAction(Action<string> CallBack)
 {
     string message = await IOWrapperAsync();
