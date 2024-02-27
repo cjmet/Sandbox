887671fb0272cc592ca8c6e873bb9a4cdaf96e26
CJ Metcalfe <khaaiev@gmail.com>
2024-02-27 16:59:07 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
 ﻿
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
+//Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalizedResources));
 
+//var list = new List<string>();
+//var random = new Random();
+//var array = list.ToArray();
+//random.Shuffle(array);
+//list = array.ToList();
 
- Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
-Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalizedResources));
-
-
-
-
-var list = new List<string>();
-var random = new Random();
-var array = list.ToArray();
-random.Shuffle(array);
-list = array.ToList();
-
-
+int i = 1;
+int j = 3;
+double k = (double) i / j;
+Console.WriteLine($"{k} => {k:P}");
 
