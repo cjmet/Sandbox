26bdba9658d2a12c94eeb32be25ac3b9652309b7
CJ Metcalfe <khaaiev@gmail.com>
2024-03-02 19:13:29 -0500

    Work more on Advanced Search
    Most Important Changes:
    
    1. The `MinimumLogLevel` constant in `Constants.cs` was changed from `2` to `0`, which means the logging level was changed from `Information` to `Trace`. This change will allow for more detailed logging, which can be useful for debugging and understanding the system's behavior.
    
    2. An additional option `Album` was added to the `Picker.ItemsSource` in `MainPage.xaml`, which means users can now select `Album` as a search criterion. This change enhances the user experience by providing more search options.
    
    3. The `AdvancedSearchBar_SearchButtonPressed` method in `MainPage.xaml.cs` was significantly updated. The changes include adding more detailed logging, handling null search text, and implementing advanced search functionality based on different search criteria and actions. This change improves the search functionality and makes the application more robust.
    
    List of Changes:
    
    1. The `MinimumLogLevel` constant in `Constants.cs` was changed from `2` to `0` (Code Change 1).
    2. An additional option `Album` was added to the `Picker.ItemsSource` in `MainPage.xaml` (Code Change 2).
    3. The `using` directives in `MainPage.xaml.cs` were updated. `System.Linq` and `Microsoft.VisualBasic` were added, and `CommonNet8` was moved to a different line (Code Change 3).
    4. The `AdvancedSearchBar_SearchButtonPressed` method in `MainPage.xaml.cs` was significantly updated (Code Change 4).
    5. The `README.md` file was updated with new project questions and a new entry in the Dev Blog about working on the Advanced Search feature (Code Change 5).
    6. The `Program.cs` file was updated with new `using` directives, new variables, and a new debug block of code (Code Change 6).
    7. The `MainPage.xaml.cs` file was updated with a new `using` directive for `Microsoft.VisualBasic` (Code Change 7).

--- Program.cs
+++ Program.cs
@@ -1,15 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DataLibrary;
+using System.Diagnostics;
+using AngelHornetLibrary;
+using static AngelHornetLibrary.AhLog;
 
+LogTrace("");
 
+var _rock = "rockies";
+var _roll = "Song1";
 
+IQueryable<Song>? _selectionSet = new List<Song>().AsQueryable();
+
+_rock = _rock.ToLower();
+_roll = _roll.ToLower();
 var _db = new PlaylistContext();
-var _playlist = _db.Songs.Where(s => s.Title.Contains("Acoustic"));
-var _songs = _db.Songs.Where(s => s.Title.Contains("Rock"));
-var _results = _songs;
+Console.WriteLine($"Debug[732] DB:{_db.Songs.Count()}");
 
+{ // Debug
+    Console.WriteLine("Debug[738] and Breakpoint");
+    var _select = _db.Songs.Where(s => s.Id < 0).DefaultIfEmpty();      // This should always be an empty song, and type IQueryable<Song?>?, but not null.
 
-_results = _playlist.Union(_songs);         // OR
-_results = _playlist.Intersect(_songs);     // AND
-_results = _playlist.Except(_songs);        // NOT
+    Console.WriteLine($"Debug[740] First_Set:{_select.Count()}");
 
+    var _test = _db.Songs.Where(s => s.Title.ToLower().Contains(_roll)).DefaultIfEmpty();
+    Console.WriteLine($"Debug[741] SecondSet:{_test.Count()}");
+
+    var _res = _select.Union(_test);
+    Console.WriteLine($"Debug[744] Union:{_res.Count()}");
+
+    foreach (var _song in _res)
+    {
+        if (_song == null)
+        {
+            Console.WriteLine($"Debug[748] Song:NULL");
+            continue;
+        }
+        Console.WriteLine($"Debug[748] Song[{_song.Id}]: {_song.Title}");
+    }
+
+    Console.WriteLine($"Debug[744] Union:{_res.Count()}");
+
+    _res = _select.Intersect(_test);
+    Console.WriteLine($"Debug[744] Intersect:{_res.Count()}");
+
+    _res = _select.Except(_test);
+    Console.WriteLine($"Debug[744] Except:{_res.Count()}");
+
+} // /Debug
